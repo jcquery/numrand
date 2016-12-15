da3a7a4db96b7bb69f6cc2680a7cc19f0e51a465
John Carney <jcarney000@gmail.com>
2016-12-14 17:12:13 -0800

    update name

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ exports.handler = function(event, context, callback) {
 
 var handlers = {
   'LaunchRequest': function() {
-    this.attributes['speechOutput'] = 'Welcome to Numrand! Give me two numbers and I\'ll randomly choose one between them.';
+    this.attributes['speechOutput'] = 'Welcome to Num Rand! Give me two numbers and I\'ll randomly choose one between them.';
     this.attributes['repromptOutput'] = 'Sorry, I didn\'t catch that. Can you repeat the two numbers?';
 
     this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptOutput']);
